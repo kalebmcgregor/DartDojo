de4ff08b4876b3db249175a3feb41271b6c5a8d8
Kaleb McGregor <kaleb.mcgregor@gmail.com>
2014-08-03 02:35:55 -0700

    Broke something on Solitaire.java. Going to bed. SHITSUCKS

--- app/src/main/java/com/example/kaleb_000/dartdojo/Solitaire.java
+++ app/src/main/java/com/example/kaleb_000/dartdojo/Solitaire.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import Android.R;
 
 import com.example.kaleb_000.dartdojo.R;
 
@@ -45,6 +47,9 @@ public class Solitaire extends Activity {
         int i = view.getId();
         switch(i){
             case R.id.button:
+                Textview textView = (TextView) findViewById(R.id.Score);
+                textView.setTextSize(40);
+                textView.
 
                 break;
             case R.id.button2:
