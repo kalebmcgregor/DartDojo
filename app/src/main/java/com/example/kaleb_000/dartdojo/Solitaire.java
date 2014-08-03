4c0f53b733396160617770699340200e335054d1
Kaleb McGregor <kaleb.mcgregor@gmail.com>
2014-08-02 22:31:53 -0700

    Added functionality to go from main activity to solitaire page

--- app/src/main/java/com/example/kaleb_000/dartdojo/Solitaire.java
+++ app/src/main/java/com/example/kaleb_000/dartdojo/Solitaire.java
@@ -4,26 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
+import com.example.kaleb_000.dartdojo.R;
 
-
-public class Main extends Activity {
+public class Solitaire extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //hide notification bar
-
+        setContentView(R.layout.activity_solitaire);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.solitaire, menu);
         return true;
     }
 
@@ -37,11 +32,5 @@ public class Main extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    //Send Current Activity to Solitaire
-    public void send_to_solitaire (View view) {
-        Intent intent = new Intent(this, Solitaire.class);
-        startActivity(intent);
     }
 }
